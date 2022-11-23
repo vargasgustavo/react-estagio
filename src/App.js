e2ce791c3684ed62128847d823cb14548a0d0154
vargasgustavo <gustavo.neumann.v@gmail.com>
2022-11-23 10:10:05 -0300

    React de um simples estagiário

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const optionsestados = [
+  {
+    label: "Minas Gerais",
+    value: "mg",
+  },
+  {
+    label: "Sâo Paulo",
+    value: "sp",
+  },
+  {
+    label: "Espírito Santo",
+    value: "es",
+  },
+  {
+    label: "Rio de Janeiro",
+    value: "rj",
+  },
+];
+
+const optionscidades = [
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+  {
+    label: "",
+    value: "",
+  },
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      estado: "São Paulo",
+    };
+
+    this.state = {
+      cidade: "Campinas",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log("Estado Selecionado!!");
+    this.setState({ estado: e.target.value });
+    this.setState({ cidade: e.target.value });
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <div className="select-container">
+          <h1>Selecione o estado:</h1>
+          <select value={this.state.estado} onChange={this.handleChange}>
+            {optionsestados.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+          <h1>Selecione a cidade:</h1>
+          <select value={this.state.cidade} onChange={this.handleChange}>
+            {optionscidades.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
